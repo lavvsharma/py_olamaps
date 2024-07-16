c37a8b7fd1a6ca9442c977a51c9b8cabfc979518
Lav Sharma <lavsharma2016@gmail.com>
2024-07-16 20:17:13 +0530

    📝 comments updated for directions api

--- py_olamaps/resources/routing.py
+++ py_olamaps/resources/routing.py
@@ -21,67 +21,78 @@ class Routing:
                    steps: bool = True,
                    overview: str = "full",
                    languages: str = "en",
-                   traffic_metadata: bool = False):
+                   traffic_metadata: bool = False) -> dict:
         """
         Description: The Directions API is a service that provides directions between two addresses.
 
         :param origin: string
-        Destination coordinates in the format lat,lng e.g: 12.993103152916301,77.54332622119354
+        Destination coordinates in the format lat,lng.
+        Example: 12.993103152916301,77.54332622119354
 
         :param destination: string
-        Origin coordinates in the format lat,lng e.g: 12.972006793201695,77.5800850011884
+        Origin coordinates in the format lat,lng.
+        Example: 12.972006793201695,77.5800850011884
 
         :param waypoints: string
-        Waypoints joined by separator (currently '|'). supported as lat,lng pairs - 12.938399,77.632873|12.938041,77.628285
+        Waypoints joined by separator (currently '|'). supported as lat,lng pairs.
+        Example: 12.938399,77.632873|12.938041,77.628285
         Default value: None
 
-        :param x_request_id:
-        A UUIDv4 unique to that HTTP request and response combination
+        :param x_request_id: string
+        A UUIDv4 unique to that HTTP request and response combination.
         Default value: None
 
-        :param x_correlation_id:
-        A UUIDv4 unique over a series of requests and responses, identifying a transaction
+        :param x_correlation_id: string
+        A UUIDv4 unique over a series of requests and responses, identifying a transaction.
         Default value: None
 
-        :param alternatives:
+        :param alternatives: boolean
         True if multiple routes are needed between source and destination.
         Default value: false
 
-        :param steps:
+        :param steps: boolean
         True if steps are needed between source and destination.
         Default value: true
 
-        :param overview:
+        :param overview: string
         Overview geometry either full, simplified or false according to highest zoom level it could be display on, or not at all.
         Default value: full
 
-        :param languages:
+        :param languages: string
         Language in which the response is expected. At the moment following languages are supported - kn (Kannada), en (English) and hi (Hindi).
         Default value: en
 
-        :param traffic_metadata:
+        :param traffic_metadata: boolean
         If this field is true, traffic metadata would be sent in the response which will contain type of congestion. (travel_advisory data will only come in response if overview is passed as "full").
         Default value: false
-        :return:
+
+        :return: dict
         """
         try:
+            query_params = dict()
+            headers = dict()
+
             directions_api_url = Api.Protocol.value + Api.Host.value + Api.Routing_Directions_Endpoint.value
 
-            query_params = {
-                "api_key": self._api_key,
-                "origin": origin,
-                "destination": destination,
-                "alternatives": alternatives,
-                "steps": steps,
-                "overview": overview,
-                "languages": languages,
-                "traffic_metadata": traffic_metadata,
-                **({"waypoints": waypoints} if waypoints is not None else {}),
-                **({"x_request_id": x_request_id} if x_request_id is not None else {}),
-                **({"x_correlation_id": x_correlation_id} if x_correlation_id is not None else {})
-            }
+            query_params["api_key"] = self._api_key
+            query_params["origin"] = origin
+            query_params["destination"] = destination
+            query_params["alternatives"] = alternatives
+            query_params["steps"] = steps
+            query_params["overview"] = overview
+            query_params["languages"] = languages
+            query_params["traffic_metadata"] = traffic_metadata
 
-            response = requests.post(directions_api_url, params=query_params)
+            if waypoints is not None:
+                query_params["waypoints"] = waypoints
+
+            if x_request_id is not None:
+                headers["x_request_id"] = x_request_id
+
+            if x_correlation_id is not None:
+                headers["x_correlation_id"] = x_correlation_id
+
+            response = requests.post(directions_api_url, headers=headers, params=query_params)
 
             if response.status_code == HTTPStatus.OK:
                 return response.json()
