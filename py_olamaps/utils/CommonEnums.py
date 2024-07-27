b4c13f53dc58c4e92f30d4b2006683e88d92378a
Lav Sharma <lavsharma2016@gmail.com>
2024-07-27 22:30:04 +0530

    👽️ oauth and map tiles endpoint added

--- py_olamaps/utils/CommonEnums.py
+++ py_olamaps/utils/CommonEnums.py
@@ -8,3 +8,15 @@ class Api(Enum):
     Places_Autocomplete_Endpoint = "/places/v1/autocomplete"
     Places_Geocode_Endpoint = "/places/v1/geocode"
     Places_Reverse_Geocode_Endpoint = "/places/v1/reverse-geocode"
+    Get_Tile_Endpoint = "/tiles/vector/v1/data"
+    Get_Map_Endpoint = "/tiles/vector/v1/styles.json"
+    Get_Style_Endpoint = "/tiles/vector/v1/styles"
+    Get_Glyph_Endpoint = "/tiles/vector/v1/fonts"
+
+
+class OAuth(Enum):
+    Protocol = "https://"
+    Host = "account.olamaps.io"
+    Route = "/realms/olamaps/protocol/openid-connect/token"
+    Grant_Type = "client_credentials"
+    Scope = "openid"
