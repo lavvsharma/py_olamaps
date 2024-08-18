81ab88b0775b5dd82632495add719293b69fd980
Lav Sharma <lavsharma2016@gmail.com>
2024-08-18 12:47:19 +0530

    ♻️ api endpoints are now divided into individual classes

--- py_olamaps/utils/CommonEnums.py
+++ py_olamaps/utils/CommonEnums.py
@@ -4,14 +4,6 @@ from enum import Enum
 class Api(Enum):
     Protocol = "https://"
     Host = "api.olamaps.io"
-    Routing_Directions_Endpoint = "/routing/v1/directions"
-    Places_Autocomplete_Endpoint = "/places/v1/autocomplete"
-    Places_Geocode_Endpoint = "/places/v1/geocode"
-    Places_Reverse_Geocode_Endpoint = "/places/v1/reverse-geocode"
-    Get_Tile_Endpoint = "/tiles/vector/v1/data"
-    Get_Map_Endpoint = "/tiles/vector/v1/styles.json"
-    Get_Style_Endpoint = "/tiles/vector/v1/styles"
-    Get_Glyph_Endpoint = "/tiles/vector/v1/fonts"
 
 
 class OAuth(Enum):
@@ -20,3 +12,34 @@ class OAuth(Enum):
     Route = "/realms/olamaps/protocol/openid-connect/token"
     Grant_Type = "client_credentials"
     Scope = "openid"
+
+
+class RoutingApi(Enum):
+    Directions_Endpoint = "/routing/v1/directions"
+    Distance_Matrix_Endpoint = "/routing/v1/distanceMatrix"
+
+
+class RoadsApi(Enum):
+    Snap_To_Road_Endpoint = "/routing/v1/snapToRoad"
+    Nearest_Roads_Endpoint = "/routing/v1/nearestRoads"
+
+
+class PlacesApi(Enum):
+    Autocomplete_Endpoint = "/places/v1/autocomplete"
+    Place_Details_Endpoint = "/places/v1/details"
+    Nearby_Search_Endpoint = "/places/v1/nearbysearch"
+    Text_Search_Endpoint = "/places/v1/textsearch"
+
+
+class GeocodeApi(Enum):
+    Forward_Geocode_Endpoint = "/places/v1/geocode"
+    Reverse_Geocode_Endpoint = "/places/v1/reverse-geocode"
+
+
+class MapTilesApi(Enum):
+    Get_Array_Of_Data_Endpoint = "/tiles/vector/v1/data/{datasetName}.json"
+    Get_Map_Endpoint = "/tiles/vector/v1/styles.json"
+    Get_Style_Endpoint = "/tiles/vector/v1/styles/{styleName}/style.json"
+    Static_Map_Image_Based_On_Center_Point_Endpoint = "/tiles/v1/styles/{styleName}/static/{lon},{lat},{zoom}/{width}x{height}.{format}"
+    Static_Map_Image_Based_On_Bounding_Box_Endpoint = "/tiles/v1/styles/{styleName}/static/{minx},{miny},{maxx},{maxy}/{width}x{height}.{format}"
+    Static_Map_Image_Based_Endpoint = "/tiles/v1/styles/{styleName}/static/auto/{width}x{height}.{format}"
